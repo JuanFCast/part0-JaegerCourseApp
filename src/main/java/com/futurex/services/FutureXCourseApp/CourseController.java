7ac3af2d4acf3343e189cb62f9b629bb13f687ad
Felipe Castillo <1006071586@u.icesi.edu.co>
2025-05-11 23:25:50 -0500

    enhance: add manual OpenTelemetry tracing to controller endpoints
    Injected Tracer and added manual span creation in controller methods to enable detailed trace collection for key HTTP endpoints.

--- src/main/java/com/futurex/services/FutureXCourseApp/CourseController.java
+++ src/main/java/com/futurex/services/FutureXCourseApp/CourseController.java
@@ -1,5 +1,7 @@
 package com.futurex.services.FutureXCourseApp;
 
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,32 +14,41 @@ public class CourseController {
     @Autowired
     private CourseRepository courseRepository;
 
+    @Autowired
+    private Tracer tracer;
+
     @RequestMapping("/")
     public String getCourseAppHome() {
-
-        return ("Course App Home");
+        Span span = tracer.spanBuilder("getCourseAppHome").startSpan();
+        try {
+            return "Course App Home";
+        } finally {
+            span.end();
+        }
     }
 
     @RequestMapping("/courses")
     public List<Course> getCourses() {
-
-        return courseRepository.findAll();
+        Span span = tracer.spanBuilder("getCourses").startSpan();
+        try {
+            return courseRepository.findAll();
+        } finally {
+            span.end();
+        }
     }
 
     @RequestMapping("/{id}")
-    public Course getSpecificCourse(@PathVariable("id") BigInteger id ) {
+    public Course getSpecificCourse(@PathVariable("id") BigInteger id) {
         return courseRepository.getOne(id);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value="/courses")
+    @RequestMapping(method = RequestMethod.POST, value = "/courses")
     public void saveCourse(@RequestBody Course course) {
         courseRepository.save(course);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE,value = "{id}")
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
     public void deleteCourse(@PathVariable BigInteger id) {
         courseRepository.deleteById(id);
     }
-
-
 }
